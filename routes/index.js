cae7ff8c3a7faf4bdbcdca10a2ab31618d560f31
dany gbelidji <dany.gbelidji@live.fr>
2021-04-06 14:14:02 +0200

    🗃 ADD - database connection

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+
+/// Code de la doc à jour : https://www.npmjs.com/package/mongodb
+
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'mesClients';
+// Use connect method to connect to the server
+client.connect(function (err) {
+  assert.strictEqual(null, err);
+  console.log('Connected successfully to server');
+
+  const db = client.db(dbName);
+
+  client.close();
+});
