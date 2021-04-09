3942222782fe3fe551b6c9efc857a735d99d4bdc
dany gbelidji <dany.gbelidji@live.fr>
2021-04-09 22:40:53 +0200

    🚧 MOD : form route to DB

--- routes/addUser.js
+++ routes/addUser.js
@@ -3,16 +3,24 @@ var router = express.Router();
 
 // const newUser = require('../public/javascripts/form')
 
-/* GET users listing. */
+
+
+/* get users listing. */
 router.get('/', function (req, res, next) {
 
     // console.log(newUser)
-
     // console.log(inputForm)
     res.render('addUser', {
         title: 'Inscription'
     });
 });
+//Requete post => ne s'affiche pas lors de la génération de la page
+router.post('/addUser', function (req, res, next) {
+
+    console.log(req)
+    res.end()
+});
+
 
 module.exports = router;
 // je veux ajouter un utilisateur à ma base de donnée
