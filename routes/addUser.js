6b45adce5f9cb39137922b83e3c9d42bd932ff87
dany gbelidji <dany.gbelidji@live.fr>
2021-04-09 11:16:14 +0200

    ✨ ADD - users form

--- routes/addUser.js
+++ routes/addUser.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.render('addUser', {
-        title: 'Ajouter un utilisateur'
+        title: 'Inscription'
     });
 });
 
